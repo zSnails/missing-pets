f40f593b03e789408b9b72e53437a65ff1b88821
zSnails <erizojuan33@gmail.com>
2025-01-17 16:58:24 -0600

    feat: register the api router

--- cmd/missing-pet-entry-point/main.go
+++ cmd/missing-pet-entry-point/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"encoding/gob"
+	"net/http"
 	"regexp"
 
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
+	"github.com/zSnails/missing-pet-tracker/api"
+	"github.com/zSnails/missing-pet-tracker/storage"
 )
 
 var reg = regexp.MustCompile(`[^a-zA-Z0-9áéíóúÁÉÍÓÚ]`)
@@ -13,7 +20,13 @@ func removeSpecialCharacters(input string) string {
 	return reg.ReplaceAllString(input, "")
 }
 
+func init() {
+	godotenv.Load()
+	gob.Register(storage.PetOwner{})
+}
+
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
 	sql.Register("sqlite_custom", &sqlite3.SQLiteDriver{
 		Extensions: []string{},
 		ConnectHook: func(conn *sqlite3.SQLiteConn) error {
@@ -26,4 +39,10 @@ func main() {
 	}
 	defer db.Close()
 
+	router := mux.NewRouter()
+	api.Register(router, storage.New(db))
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
 }
