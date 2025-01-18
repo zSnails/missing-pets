645507667947463671fe05085f07c9be0ced47f6
zSnails <erizojuan33@gmail.com>
2025-01-17 23:51:52 -0600

    feat: get single pet handler

--- api/pets/list.go
+++ api/pets/list.go
@@ -10,9 +10,37 @@ import (
 	"github.com/zSnails/missing-pet-tracker/storage"
 )
 
+func GetPet(q *storage.Queries) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		usrData := r.Context().Value("user-data").(storage.CreateUserRow)
+		vars := mux.Vars(r)
+		petId, _ := strconv.ParseInt(vars["petId"], 10, 64)
+
+		pet, err := q.GetPetByOwnerAndId(r.Context(), storage.GetPetByOwnerAndIdParams{
+			ID:      petId,
+			OwnerID: usrData.ID,
+		})
+		if err != nil {
+			log.Errorln(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(response.Response[storage.GetPetByOwnerAndIdRow]{
+			Code: http.StatusOK,
+			Data: pet,
+		})
+		if err != nil {
+			log.Errorln(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
 func ListMyPets(q *storage.Queries) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		usrData := r.Context().Value("user-data").(storage.PetOwner)
+		usrData := r.Context().Value("user-data").(storage.CreateUserRow)
 		listPets(w, r, q, usrData.ID)
 	})
 }
