2fcbcfb65047f5df4a3943b859e2d6237326dcc8
zSnails <erizojuan33@gmail.com>
2025-01-17 23:46:13 -0600

    refactor: rethink the entire routes design

--- api/routes.go
+++ api/routes.go
@@ -1,25 +1,62 @@
 package api
 
 import (
+	"context"
+	"database/sql"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"github.com/zSnails/missing-pet-tracker/api/auth"
+	"github.com/zSnails/missing-pet-tracker/api/auth/cookies"
 	"github.com/zSnails/missing-pet-tracker/api/pets"
 	"github.com/zSnails/missing-pet-tracker/storage"
 )
 
 var log = logrus.WithField("service", "api")
 
-func Register(r *mux.Router, q *storage.Queries) {
+func Register(r *mux.Router, q *storage.Queries, db *sql.DB) {
 	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Errorf("route not found %s\n", r.URL.Path)
 		http.Error(w, "Not Found", http.StatusNotFound)
 	})
-	r.Handle("/api/users/{id}/pets", pets.RegisterPet(q)).Methods("POST")
-	// TODO: other crud operations
 
 	r.Handle("/api/auth/login", auth.Login(q)).Methods("POST")
+	r.Handle("/api/auth/login", auth.Logout(q)).Methods("DELETE")
 	r.Handle("/api/auth/register", auth.Register(q)).Methods("POST")
+
+	{
+		api := r.PathPrefix("/api").Subrouter()
+		api.Use(authenticated)
+		api.Handle("/me/pets", pets.RegisterUserPet(q, db)).Methods("POST")
+		api.Handle("/me/pets", pets.ListMyPets(q)).Methods("GET")
+		api.Handle("/me/pets/{petId}", pets.GetPet(q)).Methods("GET")
+		api.Handle("/users/{id}/pets", pets.ListUserPets(q)).Methods("GET")
+        api.Handle("/me/pets/{petId}", pets.RemoveUserPet(q)).Methods("DELETE")
+	}
+
+}
+
+func authenticated(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sess, err := cookies.Store.Get(r, "Session")
+		if err != nil {
+			log.Errorln(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if sess.IsNew {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		usrData, ok := sess.Values["user-data"].(storage.CreateUserRow)
+		if !ok {
+			log.Errorln("User is not authenticated")
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		h.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), "user-data", usrData)))
+	})
 }
