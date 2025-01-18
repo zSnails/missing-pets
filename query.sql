343352282b4e0a3b223aea68eac088a6260c57cf
zSnails <erizojuan33@gmail.com>
2025-01-17 23:53:22 -0600

    refactor: make these match the schema

--- query.sql
+++ query.sql
@@ -1,20 +1,17 @@
 -- name: CreateMissingPet :one
 INSERT INTO
-missing_pets (name, type, last_seen)
-VALUES (?, ?, ?)
-RETURNING *;
+missing_pets (name, type, last_seen, owner_id)
+VALUES (?, ?, ?, ?)
+RETURNING id, name, type, last_seen;
 
 -- name: FindMissingPetsByName :many
-SELECT * FROM
+SELECT id, name, type, last_seen FROM
 missing_pets
 WHERE remove_special_characters(name)
 LIKE remove_special_characters(CAST(sqlc.arg(name) AS TEXT));
 
--- name: LinkPetAndOwner :one
-INSERT INTO
-missing_pet_owner_rel (missing_pet_id, pet_owner_id)
-VALUES (?, ?)
-RETURNING *;
+-- name: DoesUserOwnThePet :one
+SELECT 1 FROM missing_pets WHERE id = ? AND owner_id = ?;
 
 -- name: UploadPhoto :one
 INSERT INTO
@@ -23,10 +20,22 @@ VALUES (?, ?)
 RETURNING id;
 
 -- name: FindUserByEmail :one
-SELECT * FROM pet_owners WHERE email = ?;
+SELECT id, name, phone, email, address, hash FROM pet_owners WHERE email = ?;
+
+-- name: FindUserById :one
+SELECT id, name, phone, email, address FROM pet_owners WHERE id = ?;
 
 -- name: CreateUser :one
 INSERT INTO
 pet_owners (name, phone, email, address, hash)
 VALUES (?, ?, ?, ?, ?)
-RETURNING *;
+RETURNING id, name, phone, email, address;
+
+-- name: GetUserPets :many
+SELECT id, name, type, last_seen FROM missing_pets WHERE owner_id = ?;
+
+-- name: RemoveUserPet :exec
+DELETE FROM missing_pets WHERE id = ? AND owner_id = ?;
+
+-- name: GetPetByOwnerAndId :one
+SELECT id, name, type, last_seen FROM missing_pets WHERE id = ? AND owner_id = ?;
