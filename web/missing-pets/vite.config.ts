1dc47e774b190f1f9ee088ab2804397b725d98c7
zSnails <erizojuan33@gmail.com>
2025-01-20 02:34:17 -0600

    feat: proxy api and image endpoints to the backend server

--- web/missing-pets/vite.config.ts
+++ web/missing-pets/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: "http://localhost:8080"
+            },
+            '/images': {
+                target: "http://localhost:8080"
+            }
+        }
+    }
 })
